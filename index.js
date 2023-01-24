8b2abb405bd493acae062c029c1a25d89949424f
unknown <bahajonsroziyev@gmail.com>
2023-01-24 20:38:27 +0200

    bug fixed

--- index.js
+++ index.js
@@ -3,23 +3,10 @@ const winston = require('winston')
 const app = express()
 
 
-
-
 require('./startup/routes')(app); //working
 require('./startup/db.js')();// working
-
 require('./startup/config.js')(); //working
-
-
 require('./startup/prod.js')(app); //working
-
-
-
-
-
-
-
-
 
 
 const port = process.env.PORT || 5000;
